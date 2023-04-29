884c7fb3ff16ae368c1a1ad54e3169a7a1d081bf
Erdogan <kopuskopecik@gmail.com>
2023-04-30 00:56:54 +0200

    development mode

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python3 /backend/manage.py makemigrations
 python3 /backend/manage.py migrate
-python3 /backend/manage.py collectstatic --no-input -c
-#python3 /backend/manage.py runserver 0.0.0.0:8000
-gunicorn python_sitesi.wsgi:application --bind 0.0.0.0:8000  --worker-class gevent --worker-connections 1000 --workers 8 --timeout 30 --keep-alive 32  --error-logfile gunicorn_error.log  --log-level info
+#python3 /backend/manage.py collectstatic --no-input -c
+python3 /backend/manage.py runserver 0.0.0.0:8000
+# gunicorn python_sitesi.wsgi:application --bind 0.0.0.0:8000  --worker-class gevent --worker-connections 1000 --workers 8 --timeout 30 --keep-alive 32  --error-logfile gunicorn_error.log  --log-level info
